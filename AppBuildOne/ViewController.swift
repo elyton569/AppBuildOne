9823404b00731d0b64cb6bdc047a7d7555ac5129
Errin Lyton <elyton@hawaii.edu>
2020-02-20 12:39:55 -1000

    Finish Week 5 App Build #1 Step 4 to Step 6

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -12,11 +12,39 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var UHWO: UILabel!
     
+    @IBOutlet weak var TextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.gray
+                // Do any additional setup after loading the view, typically from a nib.
+        self.TextView.text = "The University of Hawaiʻi–West Oʻahu is a public university in Kapolei, Hawaii."
     }
 
+    @IBAction func ACMbuttonpressed(_ sender: Any)
+    {
+   
+    
+    //set textview text to about ACM text (find online)
+        self.TextView.text = "Academy for Creative Media."
 
+        
+        //change label text "About ACM"
+        self.UHWO.text = "About ACM."
+        
+    }
+    
+    @IBAction func AboutUHWO(_ sender: Any)
+    {
+    
+        
+    //set textview text to about UHWO text (find online)
+        self.TextView.text = "University of Hawaii West Oahu."
+        
+        
+        //change label text "About UHWO
+        self.UHWO.text = "About UHWO."
+    }
+    
 }
+
 
